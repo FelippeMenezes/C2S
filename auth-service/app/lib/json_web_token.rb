250ce4557c23a6230d909346bf6b9009f3138b1e
fsantana-springbot <fsantana@springbot.com>
2026-02-25 09:17:09 -0300

    Create JWT class and config

---
+++ auth-service/app/lib/json_web_token.rb
@@ -0,0 +1,16 @@
+class JsonWebToken
+  # Usamos a chave que definimos no docker-compose anteriormente
+  SECRET_KEY = ENV.fetch('JWT_SECRET_KEY', Rails.application.secret_key_base)
+
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def self.decode(token)
+    decoded = JWT.decode(token, SECRET_KEY)[0]
+    HashWithIndifferentAccess.new decoded
+  rescue JWT::ExpiredSignature, JWT::DecodeError
+    nil
+  end
+end
