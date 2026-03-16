151cdab9646f5306305bd5c204984fabb1889812
fsantana-springbot <fsantana@springbot.com>
2026-03-16 12:08:07 -0300

    Authentication Middleware configured

--- notification-service/app/controllers/application_controller.rb
+++ notification-service/app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
+  before_action :authorize_request
+
+  private
+
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+
+    begin
+      @decoded = JsonWebToken.decode(header)
+      if @decoded
+        @current_user_id = @decoded[:user_id] || @decoded["user_id"]
+      end
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+
+    render json: { error: 'Não autorizado' }, status: :unauthorized unless @current_user_id
+  end
 end
